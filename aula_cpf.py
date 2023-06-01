2fa0417d009e40f059bfe6906d6e6497c2d30982
JEAN CARLOS DA SILVA ANDRADE <13748584717@pmc.local>
2023-06-01 13:32:31 -0300

    validando cpf

---
+++ aula_cpf.py
@@ -0,0 +1,17 @@
+cpf_str = input('Digite o seu CPF: \n')
+cpf_str = cpf_str.replace('.','')
+valor = 0
+multiplica = 10
+for i in cpf_str[0:9]:
+    valor = valor + (int(i)*multiplica)
+    print(valor, i, multiplica, int(i)*multiplica)
+    multiplica = multiplica-1
+
+valor = valor * 10 
+print(valor)   
+valor = valor % 11
+
+if valor > 9:
+    print('resultado = 0')
+else:
+    print('resolvado é o valor da conta: ', valor)
