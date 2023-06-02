4f08b1bdcf4914648f086475dbf739376df76bc2
JEAN CARLOS DA SILVA ANDRADE <13748584717@pmc.local>
2023-06-02 14:04:41 -0300

    validando os dois digitos do cpf

--- aula_cpf.py
+++ aula_cpf.py
@@ -1,17 +1,26 @@
-cpf_str = input('Digite o seu CPF: \n')
-cpf_str = cpf_str.replace('.','')
-valor = 0
+import os
+
+cpf_str = input('Digite os 9 primeiros digitos do seu cpf: \n')
+cpf_str = cpf_str.replace('.','').replace('-','')
+valor1 = 0
+valor2 = 0
 multiplica = 10
-for i in cpf_str[0:9]:
-    valor = valor + (int(i)*multiplica)
-    print(valor, i, multiplica, int(i)*multiplica)
-    multiplica = multiplica-1
+for index, i in enumerate(cpf_str[0:9]):
+    valor1 = valor1 + (int(i)*multiplica)
+    multiplica = multiplica-1 if index < 8 else multiplica+9
 
-valor = valor * 10 
-print(valor)   
-valor = valor % 11
+valor1 = valor1 * 10  
+valor1 = valor1 % 11
+digito1 = valor1 if valor1 <= 9 else 0
+cpf = cpf_str + str(digito1)
 
-if valor > 9:
-    print('resultado = 0')
-else:
-    print('resolvado é o valor da conta: ', valor)
+for i in cpf:
+    valor2 = valor2 + (int(i)*multiplica)
+    multiplica -= 1
+
+valor2 = valor2 * 10  
+valor2 = valor2 % 11
+
+digito2 = valor2 if valor1 <= 9 else 0
+cpf = cpf + str(digito2)
+print(f'O Primeiro digito do seu CPF é {digito1}, e o segundo é {digito2} e seu cpf é {cpf}')
