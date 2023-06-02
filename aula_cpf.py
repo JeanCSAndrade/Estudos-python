93e4f4565ed4a04578b8c53314f8de8f432c3237
JeanCSAndrade <131497558+JeanCSAndrade@users.noreply.github.com>
2023-06-02 20:43:13 -0300

    removendo string do input

--- aula_cpf.py
+++ aula_cpf.py
@@ -1,7 +1,7 @@
 import os
+import re
 
-cpf_str = input('Digite os 9 primeiros digitos do seu cpf: \n')
-cpf_str = cpf_str.replace('.','').replace('-','')
+cpf_str = re.sub(r'[^0-9]','', input('Digite os 9 primeiros digitos do seu cpf: \n'))[:9]
 valor1 = 0
 valor2 = 0
 multiplica = 10
@@ -9,6 +9,7 @@ for index, i in enumerate(cpf_str[0:9]):
     valor1 = valor1 + (int(i)*multiplica)
     multiplica = multiplica-1 if index < 8 else multiplica+9
 
+print(valor1)
 valor1 = valor1 * 10  
 valor1 = valor1 % 11
 digito1 = valor1 if valor1 <= 9 else 0
@@ -18,6 +19,7 @@ for i in cpf:
     valor2 = valor2 + (int(i)*multiplica)
     multiplica -= 1
 
+print(valor2)
 valor2 = valor2 * 10  
 valor2 = valor2 % 11
 
