b40a40aa675df5c8173386dd20dabc0013eac585
JeanCSAndrade <131497558+JeanCSAndrade@users.noreply.github.com>
2023-06-03 13:01:49 -0300

    Criando gerador de cpf com função

--- aula_cpf.py
+++ aula_cpf.py
@@ -1,30 +1,21 @@
-import os
 import random as r
+
+def calcula_cpf(cpf, multiplicador):
+    valor = 0
+    for i in cpf:
+        valor += int(i)*multiplicador
+        multiplicador -= 1
+    valor *= 10
+    valor %= 11
+    valor = valor if valor <= 9 else 0
+    return valor
 
 cpf_str = ''
 for i in range(9):
     cpf_str += str(r.randint(0,9))
-print(cpf_str)
 
-valor1 = 0
-valor2 = 0
-multiplica = 10
-for index, i in enumerate(cpf_str[0:9]):
-    valor1 = valor1 + (int(i)*multiplica)
-    multiplica = multiplica-1 if index < 8 else multiplica+9
-
-valor1 = valor1 * 10  
-valor1 = valor1 % 11
-digito1 = valor1 if valor1 <= 9 else 0
-cpf = cpf_str + str(digito1)
-
-for i in cpf:
-    valor2 = valor2 + (int(i)*multiplica)
-    multiplica -= 1
-
-valor2 = valor2 * 10  
-valor2 = valor2 % 11
-
-digito2 = valor2 if valor2 <= 9 else 0
-cpf = cpf + str(digito2)
+digito1 = calcula_cpf(cpf_str, 10)
+cpf_str += str(digito1)
+digito2 = calcula_cpf(cpf_str, 11)
+cpf = cpf_str+str(digito2)
 print(f'O Primeiro digito do seu CPF é {digito1}, e o segundo é {digito2} e seu cpf é {cpf}')
