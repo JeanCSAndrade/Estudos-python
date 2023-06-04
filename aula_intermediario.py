d30501e3c9f7b18dd9933153a39c5c4f44e94f38
JeanCSAndrade <131497558+JeanCSAndrade@users.noreply.github.com>
2023-06-04 15:20:02 -0300

    aprendendo funcao com adiamento de execucao

--- aula_intermediario.py
+++ aula_intermediario.py
@@ -1,9 +1,40 @@
-#Iniciando
-def soma(*args):
-    soma = 0
-    for n in args:
-        soma += n
-        print('total e: ', soma, n)
-    return soma
+# import random as r
+# def mult(*args):
+#     multiplicados = 1
+#     for n in args:
+#         multiplicados *= n
+#         print(f'numero é {n} total e: {multiplicados} {par(multiplicados)}')
+#     return multiplicados
 
-print(soma(1,2,3,4,5,6,7,8,9))
+# def par(*args):
+#     for n in args:
+#         par = n % 2 ==0
+#         if par:
+#             resultado = 'E é um numero par'
+#             return resultado
+#         resultado = 'E é um numero impar'
+#         return resultado
+         
+# valores = []
+# for _ in range(9):
+#     valores.append(r.randint(1, 9))
+# multiplicados = mult(*valores)
+import datetime as dt
+def saudacao(nome):
+    hora = dt.datetime.now().time()
+    hora = hora.hour
+    time = ''
+    if hora >= 5 and hora < 12:
+        time = 'Bom dia'
+    elif hora >= 12 and hora < 18:
+        time = 'Boa tarde'
+    else:
+        time = 'Boa noite'
+
+    def saudar():
+        return f'{time}, {nome}'
+    return saudar
+
+comprimentar1 = saudacao('Jean')
+comprimentar2 = saudacao('Brian')
+print(comprimentar1())
