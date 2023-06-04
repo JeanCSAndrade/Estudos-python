ad7b01edf0c5d9149559b8f6a5335ddf1659fe45
JeanCSAndrade <131497558+JeanCSAndrade@users.noreply.github.com>
2023-06-04 19:11:24 -0300

    estudando

--- aula_intermediario.py
+++ aula_intermediario.py
@@ -1,3 +1,4 @@
+# Criando funções com varios argumentos
 # import random as r
 # def mult(*args):
 #     multiplicados = 1
@@ -19,22 +20,49 @@
 # for _ in range(9):
 #     valores.append(r.randint(1, 9))
 # multiplicados = mult(*valores)
-import datetime as dt
-def saudacao(nome):
-    hora = dt.datetime.now().time()
-    hora = hora.hour
-    time = ''
-    if hora >= 5 and hora < 12:
-        time = 'Bom dia'
-    elif hora >= 12 and hora < 18:
-        time = 'Boa tarde'
-    else:
-        time = 'Boa noite'
 
-    def saudar():
-        return f'{time}, {nome}'
-    return saudar
 
-comprimentar1 = saudacao('Jean')
-comprimentar2 = saudacao('Brian')
-print(comprimentar1())
+# criando funções com adiamento de chamada
+# import datetime as dt
+# def saudacao(nome):
+#     hora = dt.datetime.now().time()
+#     hora = hora.hour
+#     time = ''
+#     if hora >= 5 and hora < 12:
+#         time = 'Bom dia'
+#     elif hora >= 12 and hora < 18:
+#         time = 'Boa tarde'
+#     else:
+#         time = 'Boa noite'
+
+#     def saudar():
+#         return f'{time}, {nome}'
+#     return saudar
+
+# comprimentar1 = saudacao('Jean')
+# comprimentar2 = saudacao('Brian')
+# print(comprimentar1())
+
+# exercicio de funções com multiplas funções e adiamento de chamada
+# def multiplicador(muliplicador):
+#     def calcular(numero):
+#         return numero * muliplicador 
+#     return calcular
+
+# dobro = multiplicador(2)
+# triplo = multiplicador(3)
+# quadruplo = multiplicador(4)
+
+# print(dobro(5))
+# print(triplo(5))
+# print(quadruplo(5))
+
+
+#inciiando estudos de dicionarios
+
+dict1 = {'Nome': 'Jean', 'Sobrenome': 'Andrade'}
+dict1['Idade'] = 32
+
+for tupla in dict1.items():
+    for itens in tupla:
+        print(itens)
