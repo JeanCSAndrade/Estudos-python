18cdfe04e331b87b6c92c4efd97c1ac55bd3de30
JEAN CARLOS DA SILVA ANDRADE <26394@pmc.local>
2023-11-22 09:43:32 -0300

    Adicionando lista de ramais

--- aula_intermediario.py
+++ aula_intermediario.py
@@ -67,9 +67,37 @@
 #     for itens in tupla:
 #         print(itens)
 
-s1 = {1, 2, 3, 4, 5}
+# s1 = {1, 2, 3, 4, 5}
 
-s1.add('Jean')
-print(s1)
-s1.clear()
-print(s1)
+# s1.add('Jean')
+# print(s1)
+# s1.clear()
+# print(s1)
+
+# lista = 'Jean'
+# metodo = input('Qual metodo? ')
+# if hasattr(lista, metodo):
+#     print('Metodo exists')
+#     print(getattr(lista, metodo)())
+
+
+
+import tkinter as tk 
+frame = tk.Tk() 
+frame.title("TextBox Input") 
+frame.geometry('400x200') 
+def printInput(): 
+    inp = inputtxt.get(1.0, "end-1c") 
+    lbl.config(text = "Provided Input: "+inp) 
+inputtxt = tk.Text(frame, 
+                   height = 5, 
+                   width = 20) 
+  
+inputtxt.pack() 
+printButton = tk.Button(frame, 
+                        text = "Print",  
+                        command = printInput) 
+printButton.pack() 
+lbl = tk.Label(frame, text = "") 
+lbl.pack() 
+frame.mainloop() 
